4455a9cb36fda6d86f4a754eb0f5e53d1b869b7a
tanayvaswani <vaswani.tanay9@gmail.com>
2024-06-25 17:48:07 +0530

    feat: JSON response

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,14 @@ export interface Env {
 }
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		console.log('Hello, from local console!');
 		console.log(request.cf);
 
-		return new Response('Hello, Tanay!');
+		// handle input body received in request
+		// make a db request
+
+		// return new Response('Hello, Tanay!');
+		return new Response(JSON.stringify({ message: 'Hello, Tanay!' }));
 	},
 } satisfies ExportedHandler<Env>;
