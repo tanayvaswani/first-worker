0bce364a79427dfd72572c22b3492478b3a199c6
tanayvaswani <vaswani.tanay9@gmail.com>
2024-06-25 17:45:38 +0530

    chore: Env interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+export interface Env {
+	// MY_KV_NAMESPACE: KVNamespace;
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	// MY_BUCKET: R2Bucket;
+	// MY_SERVICE: Fetcher;
+	// MY_QUEUE: Queue;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		console.log('Hello, from local console!');
