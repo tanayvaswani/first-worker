67bbd032cbe7bd98e6a4aba3bc02ad34d12a31e5
tanayvaswani <vaswani.tanay9@gmail.com>
2024-06-25 18:18:00 +0530

    feat: Hono.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { Hono } from 'hono';
+
 export interface Env {
 	// MY_KV_NAMESPACE: KVNamespace;
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -6,19 +8,31 @@ export interface Env {
 	// MY_QUEUE: Queue;
 }
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		console.log('Hello, from local console!');
-		console.log(request.cf);
+// Native CF stuff
 
-		// handle input body received in request
-		// make a db request
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		console.log('Hello, from local console!');
+// 		console.log(request.cf);
 
-		// return new Response('Hello, Tanay!');
-		return new Response(JSON.stringify({ message: 'Hello, Tanay!' }), {
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-	},
-} satisfies ExportedHandler<Env>;
+// 		// handle input body received in request
+// 		// make a db request
+
+// 		// return new Response('Hello, Tanay!');
+// 		return new Response(JSON.stringify({ message: 'Hello, Tanay!' }), {
+// 			headers: {
+// 				'Content-Type': 'application/json',
+// 			},
+// 		});
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+// Using Hono.js
+
+const app = new Hono();
+
+app.get('/', (c) => {
+	return c.json({ message: 'Hello, Tanay!' });
+});
+
+export default app;
