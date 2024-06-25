8e2fe16e6a112fca8e54ec3f48b35d150c6d6f28
tanayvaswani <vaswani.tanay9@gmail.com>
2024-06-25 18:19:20 +0530

    chore: @cloudflare/ai added

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,5 @@
 import { Hono } from 'hono';
 
-export interface Env {
-	// MY_KV_NAMESPACE: KVNamespace;
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	// MY_BUCKET: R2Bucket;
-	// MY_SERVICE: Fetcher;
-	// MY_QUEUE: Queue;
-}
-
-// Native CF stuff
-
-// export default {
-// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-// 		console.log('Hello, from local console!');
-// 		console.log(request.cf);
-
-// 		// handle input body received in request
-// 		// make a db request
-
-// 		// return new Response('Hello, Tanay!');
-// 		return new Response(JSON.stringify({ message: 'Hello, Tanay!' }), {
-// 			headers: {
-// 				'Content-Type': 'application/json',
-// 			},
-// 		});
-// 	},
-// } satisfies ExportedHandler<Env>;
-
-// Using Hono.js
-
 const app = new Hono();
 
 app.get('/', (c) => {
